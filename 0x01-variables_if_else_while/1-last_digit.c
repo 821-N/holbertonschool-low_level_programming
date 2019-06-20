64bad4cce409f8516233b14330f0fba88ab5e1d8
Nathaniel <821@holbertonschool.com>
2019-06-20 13:09:21 +0000

    fix style
    uwu

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,14 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	char last = n % 10;
+
 	printf("Last digit of %d is %d and is ", n, last);
 	if (last > 5)
 		printf("greater than 5");
-	else if(last == 0)
+	else if (last == 0)
 		printf("0");
 	else
 		printf("less than 6 and not 0");
 	printf("\n");
-	
+
 	return (0);
 }
