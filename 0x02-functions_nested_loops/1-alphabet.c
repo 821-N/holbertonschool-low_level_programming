ad41c59efe373c69fcb7147cdcf8e9e68473a2c9
Nathaniel <821@holbertonschool.com>
2019-06-24 13:53:17 +0000

    Adds holberton.h 1-main.c 1-alphabet.c This is an auto-generated message
    uwu

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Output letters "abc...z\n"
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
