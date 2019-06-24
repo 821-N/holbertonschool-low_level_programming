1852b8847f9353dfc5cb8d17d18b9aa94b11b0bf
Nathaniel <821@holbertonschool.com>
2019-06-24 13:27:46 +0000

    Adds 0-holberton.c This is an auto-generated message
    uwu

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "_putchar.h"
+
+/**
+ * print - print to stdout
+ * @message: string to print
+ */
+void print(char *message)
+{
+	while (*message)
+		_putchar(*message++);
+}
+
+/**
+ *main-main
+ *Return:0
+ */
+int main(void)
+{
+	print("holberton\n");
+	return (0);
+}
+
