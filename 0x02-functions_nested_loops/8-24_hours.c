925422e9bb6c802d07faff8122281bfad4fd79b5
Nathaniel <821@holbertonschool.com>
2019-06-24 14:58:53 +0000

    Adds 8-24_hours.c This is an auto-generated message
    uwu

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_2_digits - print 2 digit number
+ *
+ * @n - number to print
+ */
+void print_2_digits(char n)
+{
+	_putchar('0' + n / 10);
+	_putchar('0' + n % 10);
+}
+
+/**
+ * jack_bauer - print times from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	char hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+		for (minute = 0; minute < 60; minute++)
+		{
+			print_2_digits(hour);
+			_putchar(':');
+			print_2_digits(minute);
+			_putchar('\n');
+		}
+}
