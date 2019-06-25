5a182415f413081451bc2bc95f192d8a94acccec
Nathaniel <821@holbertonschool.com>
2019-06-25 13:17:14 +0000

    Adds 100-times_table.c This is an auto-generated message
    uwu

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
 /**
+ * print_digit - display 1 digit of a number,
+ *  unless it is a leading 0
+ *
+ * @n: number
+ * @place: power of 10. Do not use 1.
+ */
+void print_digit(int n, int place)
+{
+	if (n >= place)
+		putchar('0' + n / place % 10);
+	else
+		putchar(' ');
+}
+
+/**
+ * print_4_digits - print without leading 0s
+ *
+ * @n: number to print
+ */
+void print_4_digits(int n)
+{
+	print_digit(n, 1000);
+	print_digit(n, 100);
+	print_digit(n, 10);
+	putchar('0' + n % 10);
+}
+
+/**
  * print_times_table - print @n*@n times table
  *
  * @n: size (0 to 15)
@@ -16,7 +44,10 @@ void print_times_table(int n)
 	{
 		putchar('0');
 		for (x = 1; x <= n; x++)
-			printf(",%4d", x * y);
+		{
+			putchar(',');
+			print_4_digits(x * y);
+		}
 		putchar('\n');
 	}
 }
