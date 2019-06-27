3a9490f531cae0583a680fc5231c2677214742a5
Nathaniel <821@holbertonschool.com>
2019-06-27 13:15:18 +0000

    Adds 0-isupper.c This is an auto-generated message
    uwu

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - checks if a character is uppercase
+ *
+ * @c: character to check
+ * Return: 1 if @c is uppercase, otherwise 0
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
