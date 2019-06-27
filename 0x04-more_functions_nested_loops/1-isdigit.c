13605c54b8b5b5e5e0c65faeef1b475da96c0016
Nathaniel <821@holbertonschool.com>
2019-06-27 13:19:04 +0000

    Adds 1-isdigit.c This is an auto-generated message
    uwu

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+/**
+ * _isdigit - check if a character is a digit
+ *
+ * @c: character
+ * Return: 1 if character is 0-9, otherwise 0
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
