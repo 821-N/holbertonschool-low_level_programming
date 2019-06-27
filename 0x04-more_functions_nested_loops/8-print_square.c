76e8a1334e033b5c9e2ad0c2b833988e277bc0cc
Nathaniel <821@holbertonschool.com>
2019-06-27 14:05:07 +0000

    Adds 8-print_square.c holberton.h This is an auto-generated message
    uwu

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_square - print square of '#'s
+ *
+ * @size: width and height of square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (j = 0; j < size; j++)
+		{
+			for (i = 0; i < size; i++)
+				_putchar('#');
+			_putchar('\n');
+		}
+}
