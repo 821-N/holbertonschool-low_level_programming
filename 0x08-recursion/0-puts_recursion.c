3cd5393949222e22377370060b7d542605de340c
Nathaniel <821@holbertonschool.com>
2019-07-10 09:29:35 -0600

    adds 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - recursive implementation of puts
+ *
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
