dae78169d7406e87bc7b3d4194466ade1cb12eb8
Nathaniel <821@holbertonschool.com>
2019-07-10 23:58:54 -0600

    replaces for loop with recursive strlen

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -16,6 +16,19 @@ int is_palindrome_sub(char *s, int len)
 }
 
 /**
+ * _strlen_recursion - recursive strlen
+ *
+ * @s: string to find length of
+ * Return: length of @s
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+
+/**
  * is_palindrome - check if string is palindrome
  *
  * @s: string
@@ -23,9 +36,5 @@ int is_palindrome_sub(char *s, int len)
  */
 int is_palindrome(char *s)
 {
-	char *end = s;
-
-	for (; *end; end++)
-		;
-	return (is_palindrome_sub(s, end - s));
+	return (is_palindrome_sub(s, _strlen_recursion(s)));
 }
