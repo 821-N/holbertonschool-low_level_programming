dfe4cf707457c955c675568d36c593db1efe8e64
Nathaniel <821@holbertonschool.com>
2019-07-13 18:10:26 -0400

    adds 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print program's filename
+ *
+ * @argc: arg count
+ * @argv: arg values
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	puts(argv[0]);
+}
