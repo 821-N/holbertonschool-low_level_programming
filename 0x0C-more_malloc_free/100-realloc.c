9ad89e13bc1327946ae10814016efd49070db535
Nathaniel <821@holbertonschool.com>
2019-07-18 15:11:37 -0400

    fixes minimum check

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,7 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!new)
 		return (NULL);
 	/* copy data to new array */
-	if (old_size < new_size)
+	if (old_size > new_size)
 		old_size = new_size;
 	while (old_size-- > 0)
 		*newer++ = *ptr2++;
