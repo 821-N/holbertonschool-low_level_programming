e2795cfce0c7e1c3db33a2b87af5257a99a3d4dd
Nathaniel <821@holbertonschool.com>
2019-07-22 14:57:09 -0400

    adds 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ *
+ * @name: name
+ * @age: age
+ * @owner: owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
