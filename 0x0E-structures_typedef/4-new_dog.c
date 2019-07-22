a9c4592e11e0a4772303bc7d9e8a23df4b36d0a4
Nathaniel <821@holbertonschool.com>
2019-07-22 15:29:09 -0400

    adds 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - create a copy of a string
+ *
+ * @str: string
+ * Return: pointer to new string, or NULL
+ */
+char *_strdup(char *str)
+{
+	unsigned int length;
+	char *new;
+
+	/* check str */
+	if (!str)
+		return (NULL);
+	for (length = 0; str[length]; length++)
+		;
+	/* allocate */
+	new = malloc(length + 1);
+	if (!new)
+		return (NULL);
+	/* copy */
+	while (length--)
+		new[length] = str[length];
+	return (new);
+}
+
+/**
+ * new_dog - for those who think dynamic memory allocation is acceptable
+ *
+ * @name: name (will be copied)
+ * @age: age
+ * @owner: owner (will be copied)
+ * Return: pointer to new dog struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog = malloc(sizeof(dog_t));
+
+	if (!dog)
+		return (NULL);
+
+	dog->age = age;
+	dog->name = _strdup(name);
+	dog->owner = _strdup(owner);
+
+	return (dog);
+}
