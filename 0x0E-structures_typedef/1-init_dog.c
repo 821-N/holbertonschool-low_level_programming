b2da175a9100b3188e978ee73e216edc56fa8b77
Nathaniel <821@holbertonschool.com>
2019-07-22 15:02:13 -0400

    adds 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - for those who refuse to use struct initializers
+ *
+ * @d: pointer to struct
+ * @name: d.name
+ * @age: d.age
+ * @owner: d.owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
