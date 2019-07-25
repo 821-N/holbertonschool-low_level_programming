8714fdc372157e2728b126bb41bebccb84782975
Nathaniel <821@holbertonschool.com>
2019-07-25 10:09:19 -0400

    fix styles

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,6 +2,15 @@
 #include "3-calc.h"
 #include <stdlib.h>
 
+/**
+ * main - main
+ *
+ * do an operation on 2 numbers
+ * a <number1> <op> <number2>
+ * @argc: arg count
+ * @argv: args
+ * Return: 98 = wrong number of args, 99 = invalid op, 100 = div by 0
+ */
 int main(int argc, char **argv)
 {
 	int (*f)(int a, int b);
@@ -17,10 +26,12 @@ int main(int argc, char **argv)
 	if (!f)
 	{
 		puts("Error");
-		return(99);
+		return (99);
 	}
 
 	res = f(atoi(argv[1]), atoi(argv[3]));
 
 	printf("%d\n", res);
+
+	return (0);
 }
