f798a76ab7462ef7e1f83b2e8a590ed5c61a4581
Nathaniel <821@holbertonschool.com>
2019-08-05 14:00:21 -0400

    add 3
    ps I don't check malloc return anymore *puts on sunglasses*

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add node to end of linked list (warning: slow!)
+ *
+ * @head: pointer to head struct pointer
+ * @n: int to add
+ * Return: pointer to new item, or NULL if failed
+ */
+listint_t *add_nodeint_end(listint_t **head, int n)
+{
+	listint_t *new, *end;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(*new));
+
+	new->n = n;
+	new->next = NULL;
+
+	if (!*head)
+		*head = new;
+	else
+	{
+		end = *head;
+		while (end->next)
+			end = end->next;
+		end->next = new;
+	}
+	return (new);
+}
