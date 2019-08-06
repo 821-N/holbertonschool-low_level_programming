f14c07fd893be94d7ea72d68aa095c75e2b247b7
Nathaniel <821@holbertonschool.com>
2019-08-06 10:36:59 -0400

    fix free listint safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,30 +2,35 @@
 #include <stdio.h>
 
 /**
- * find_loop - find a loop in a linked list
+ * last_item - find the last item in a linked list
  *  Based on code from
- *  http://geeksforgeeks.org/detect-and-remove-loop-in-a-linked-list
- * @head: pointer to start of list
- * Return: a pointer to the last unique item, or NULL if no loop exists
+ *  http://geeksforgeeks.org/detect-and-remove-loop-in-a-linked-list (method 3)
+ *  except this one actually works!
+ * @head: pointer to first item in list
+ * Return: a pointer to the last item in the list
  */
-listint_t *find_loop(listint_t *head)
+listint_t *last_item(listint_t *head)
 {
 	listint_t *slow = head, *fast = head;
 
-	if (!head || !head->next) /* list has 0 or 1 items */
+	if (!head) /* empty list */
 		return (NULL);
-
 	do {
+		if (!fast->next)
+			return (fast);
+		if (!fast->next->next)
+			return (fast->next);
+		if (slow->next == head)
+			return (slow);
 		slow = slow->next;
 		fast = fast->next->next;
-		if (!fast || !fast->next)
-			return (NULL); /* found end of list */
 	} while (slow != fast);
 	/* found loop */
-	do {
+	while (head->next != fast->next)
+	{
 		head = head->next;
 		fast = fast->next;
-	} while (head->next != fast->next);
+	}
 	return (fast);
 }
 
@@ -39,20 +44,19 @@ size_t free_listint_safe(listint_t **h)
 	listint_t *last, *next;
 	size_t size = 0;
 
-	if (!h)
+	if (!h || !*h)
 		return (0);
 
-	last = find_loop(*h);
-	if (last)
-		last->next = NULL;
+	last = last_item(*h);
 
-	last = *h;
-	while (last)
+	while (1)
 	{
-		next = last->next;
-		free(last);
-		size += sizeof(listint_t);
-		last = next;
+		next = (*h)->next;
+		size++;
+		free(*h);
+		if (*h == last)
+			break;
+		*h = next;
 	}
 
 	*h = NULL;
