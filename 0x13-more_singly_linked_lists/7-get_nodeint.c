2902a6368976eda8132f608c6e5da9e467c5a881
Nathaniel <821@holbertonschool.com>
2019-08-06 07:28:13 -0400

    betty

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (index == 0)
 		return (head);
 	if (head)
-		return get_nodeint_at_index(head->next, index-1);
+		return (get_nodeint_at_index(head->next, index - 1));
 	return (NULL);
 }
