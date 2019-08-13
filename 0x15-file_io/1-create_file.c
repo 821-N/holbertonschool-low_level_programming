ff137682a400e88d9844a57e81a40973aa1dd081
Nathaniel <821@holbertonschool.com>
2019-08-13 18:59:46 -0400

    remove binary literal :(

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = creat(filename, 0b110000000);
+	fd = creat(filename, 0600);
 	if (fd < 0)
 		return (-1);
 
