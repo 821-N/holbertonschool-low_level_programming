25ffb1c405dbe11fc8f20579bef661dcfa6b8a5e
Nathaniel <821@holbertonschool.com>
2019-08-13 18:36:57 -0400

    add 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - read and print contents of a file
+ * @filename: name of file
+ * @letters: maximum number of chars to read
+ * Return: number of chars read.
+ *  If an error occurs, returns 0
+ * ehehehehe
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *data;
+	int fd;
+	ssize_t written;
+
+	if (!filename)
+		return (0);
+
+	data = malloc(letters);
+	if (!data)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	written = read(fd, data, letters);
+	if (written < 0)
+	{
+		written = 0;
+		goto free;
+	}
+
+	if (write(STDOUT_FILENO, data, written) != written)
+	{
+		written = 0;
+		goto free;
+	}
+
+free:
+	free(data);
+	return (written);
+}
