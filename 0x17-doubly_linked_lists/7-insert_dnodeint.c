7fedc30dc0de8a44deb666c89dbacbc3e21a590c
Nathaniel <821@holbertonschool.com>
2019-09-15 21:28:30 -0400

    add 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_after - insert a node in a list after an element
+ * @h: ptr to node
+ * @n: value of new node
+ * Return: ptr to new node, or NULL if failed
+ */
+dlistint_t *add_dnodeint_after(dlistint_t *h, int n)
+{
+	dlistint_t *new;
+
+	if (!h)
+		return (NULL);
+	new = malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = h->next;
+	new->prev = h;
+	h->next->prev = new;
+	h->next = new;
+	return (new);
+}
+
+
+/**
+ * insert_dnodeint_at_index - insert node at index in list (slow)
+ * @h: head ptr
+ * @idx: index
+ * @n: number to insert
+ * Return: ptr to new node, or NULL if failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *curr;
+
+	if (!h)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	curr = *h;
+	while (--idx)
+		if (curr)
+			curr = curr->next;
+		else
+			return (NULL);
+	return (add_dnodeint_after(curr, n));
+}
+
