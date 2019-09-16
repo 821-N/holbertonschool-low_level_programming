8f74bb4460b0eeebd06cd7ef4fa192e937b85dd0
Nathaniel <821@holbertonschool.com>
2019-09-15 21:49:30 -0400

    fix deleteing head

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,6 +2,7 @@
 
 /**
  * delete_dnodeint_at_index - remove node from list (slow)
+ * If head is deleted, NEXT node becomes new head.
  * @head: address of head ptr
  * @index: index of node to remove
  * Return: 1 (success) or -1 (failure)
@@ -12,16 +13,22 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (!head)
 		return (-1);
+	/* find node */
 	curr = *head;
 	while (--index)
 		if (curr)
 			curr = curr->next;
 		else
 			return (-1);
+	/* link prev/next */
 	if (curr->prev)
 		curr->prev->next = curr->next;
 	if (curr->next)
 		curr->next->prev = curr->prev;
+	/* free */
+	if (curr == *head)
+		*head = curr->next;
 	free(curr);
+
 	return (1);
 }
