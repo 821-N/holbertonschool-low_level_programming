da257caac75bd7fb458602fcc804c2a6a6039741
Nathaniel <821@holbertonschool.com>
2019-10-10 19:11:18 -0400

    fix style

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - return bucket index for a key
+ * @key: key
+ * @size: number of buckets
+ * Return: index
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
