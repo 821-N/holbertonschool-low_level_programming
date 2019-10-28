4c3d2288bcc2fca2dab276957458826309e9957b
Nathaniel <821@holbertonschool.com>
2019-10-28 02:42:29 -0400

    fix double include

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -1,3 +1,6 @@
+#ifndef SORT_H
+#define SORT_H
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -16,3 +19,5 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
+
+#endif
