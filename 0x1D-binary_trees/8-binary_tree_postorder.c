6b5fb1980727b5260b594a215e44986812bad912
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-12-03 11:39:53 -0500

    add 8

---
+++ 0x1D-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - call a function on each node of a binary tree using
+ *  post-order traversal
+ * @tree: root of tree
+ * @func: function to call
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
