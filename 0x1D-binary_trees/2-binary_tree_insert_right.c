67b2b63100006d5feea796a2e78a9cdb5fb4d4ab
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-12-03 11:15:26 -0500

    add 2

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -2,23 +2,23 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - insert left node
- *  see binary_tree_insert_right
+ * binary_tree_insert_right - insert right node
+ *  see binary_tree_insert_left
  * @parent: node's parent
  * @value: value
  * Return: pointer to node, or NULL
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(*new));
 
 	if (!new)
 		return (NULL);
 	new->parent = parent;
-	new->left = parent->left;
-	if (parent->left)
-		parent->left->parent = new;
-	parent->left = new;
+	new->right = parent->right;
+	if (parent->right)
+		parent->right->parent = new;
+	parent->right = new;
 	new->n = value;
 	return (new);
 }
