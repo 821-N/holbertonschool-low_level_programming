6253e2f88e3a5bdc76eda2afdab85146e86db351
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-12-03 11:51:52 -0500

    add 11

---
+++ 0x1D-binary_trees/11-binary_tree_size.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - find number of nodes in tree
+ * @tree: root of tree
+ * Return: depth
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
